be47454d295aebad5400339c724bd35c2fe6e230
Abdulaziz-06 <aaziz871048@gmail.com>
2025-05-22 20:41:18 +0530

    updated

--- src/components/App1.jsx
+++ src/components/App1.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
-
+import Product from "./Product"
 export default function App1() {
   return (
+    <div>
     <div>My App1</div>
-  )
+    <Product />
+    </div>
+  );
 }
---
+++ src/components/Product.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Product() {
+  return (
+    <div><h1>This is Product</h1></div>
+  )
+}
