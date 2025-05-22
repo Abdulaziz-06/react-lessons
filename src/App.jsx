6edc420ebbed19b5b8dbfc859cc285f303d9bb1e
Abdulaziz-06 <aaziz871048@gmail.com>
2025-05-22 20:33:01 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <header>
           <h1>React app</h1>
           <Link to="/">App1</Link>-
-          <Link to="/app2">App2</Link>-
+          <Link to="/app2">App2</Link>
           
           <hr />
         </header>
